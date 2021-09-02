8f65ce19355f15688d43fa68967c763000b450ab
LwcReber <1079670310@qq.com>
2021-09-02 17:36:35 +0800

    修改client运行方式

--- client/src/main.ts
+++
@@ -1,6 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import Notifications from '@kyvg/vue3-notification'
-const app = createApp(App)
-app.use(Notifications)
-app.mount('#app')
